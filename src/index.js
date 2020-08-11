8be4fa4f657b871a029fb85b6fe5662bf59423cd
keijeizei <53002096+keijeizei@users.noreply.github.com>
2020-08-11 23:24:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App1 from './app/App1'; //Aaron's App.js
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 
