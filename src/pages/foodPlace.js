dfed37346ab9ddb554278b656463cfa9ab3e2e14
jdleonor <jairusleonor@gmail.com>
2020-08-11 19:37:53 +0800

    Add food place details page

---
+++ src/pages/foodPlace.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rate from '../components/rate';
+
+function FoodPlace() {
+  return (
+    <div>
+      <h1>foodPlace</h1>
+      <Rate />
+    </div>
+  );
+}
+
+export default FoodPlace;
