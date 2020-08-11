6e2eed3ae5126febe200f45c17b4e1d811820c1d
keijeizei <53002096+keijeizei@users.noreply.github.com>
2020-08-11 23:00:36 +0800

    Update App1.js

--- src/app/App1.js
+++ src/app/App1.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux'
 import store from '../redux/userRedux/store'
 import './App.css';
-
-import UserContainer from '../components/UserContainer'
 import CreateAccount from '../pages/CreateAccount'
 import Login from '../pages/Login'
 
@@ -11,7 +9,6 @@ function App() {
   return (
     <Provider store={store}>
     <div className="App">
-   	  <UserContainer/>
       <Login />
       <CreateAccount /><br/><br/><br/>
     </div>
