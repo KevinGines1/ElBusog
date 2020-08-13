dfaa24d4bacbba140f33d2607a38ec2aee8c36a1
keijeizei <kctan9@up.edu.ph>
2020-08-13 19:24:28 +0800

    Fix dependency warning

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -3,13 +3,13 @@ import { withRouter } from 'react-router-dom';
 
 function ScrollToTop({ history }) {
     useEffect(() => {
-
         const unlisten = history.listen(() => {
             window.scrollTo(0, 0);
         });
         return () => {
             unlisten();
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (null);
