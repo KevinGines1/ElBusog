ec10049bbc25e4b859f8e61afab47ef47af1b7a3
keijeizei <kctan9@up.edu.ph>
2020-08-23 11:49:52 +0800

    Add open and close time to food place details page

--- src/pages/FoodPlace.js
+++ src/pages/FoodPlace.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import Ratings from 'react-ratings-declarative';
 import JeepneyRoute from '../components/JeepneyRoute';
@@ -12,16 +12,66 @@ function FoodPlace({ match }) {
   const foodPlacesData = useSelector(state => state.zeit.foodPlacesData);
   const foodPlaces = foodPlacesData.listOfFoodPlaces;
   const foodPlace = foodPlaces.filter(foodPlace => foodPlace.Food_place_name === match.params.foodPlaceName);
-  const [placeClosed, setPlaceClosed] = useState(false)
+  var placeClosed = false
+  var nextOpenWhen = ""
+  var openTimeSentence = ""
 
   var date = new Date();
   var day = date.getDay();                                // day of the week (0-6 is Sunday-Saturday)
   var hour = date.getHours() * 100 + date.getMinutes()    // hour in 24-hour format (0-23)
+
   if(foodPlace[0]) {
-    console.log(foodPlace[0])
+    var openHourText = ""
+    var closeHourText = ""
+    //convert data to understandable text
+    if(foodPlace[0].Opening_time) {
+      openHourText = foodPlace[0].Opening_time.slice(0, -2) + ":" + foodPlace[0].Opening_time.slice(-2)
+      closeHourText = foodPlace[0].Closing_time.slice(0, -2) + ":" + foodPlace[0].Closing_time.slice(-2)
+    }
+    var openWeekDays = ""
+    if(foodPlace[0].Days_open.includes(0)) {
+      openWeekDays += "Su "
+    }
+    if(foodPlace[0].Days_open.includes(1)) {
+      openWeekDays += "M "
+    }
+    if(foodPlace[0].Days_open.includes(2)) {
+      openWeekDays += "T "
+    }
+    if(foodPlace[0].Days_open.includes(3)) {
+      openWeekDays += "W "
+    }
+    if(foodPlace[0].Days_open.includes(4)) {
+      openWeekDays += "Th "
+    }
+    if(foodPlace[0].Days_open.includes(5)) {
+      openWeekDays += "F "
+    }
+    if(foodPlace[0].Days_open.includes(6)) {
+      openWeekDays += "Sa"
+    }
+    //check if food place is closed
     if((hour < foodPlace[0].Opening_time || hour > foodPlace[0].Closing_time || !foodPlace[0].Days_open.includes(day)) && foodPlace[0].Opening_time && !placeClosed) {
-      console.log(hour > foodPlace[0].Closing_time, hour,foodPlace[0].Closing_time, foodPlace[0].Opening_time)
-      setPlaceClosed(true)
+      placeClosed = true
+      //check when food place will open next
+      if(hour < foodPlace[0].Opening_time && foodPlace[0].Days_open.includes(day)) {
+        nextOpenWhen = `Opens later at ${openHourText}`
+      }
+      else if(hour > foodPlace[0].Closing_time && (foodPlace[0].Days_open.includes(day+1) || day+1 === 7)) {
+        nextOpenWhen = `Opens tomorrow at ${openHourText}`
+      }
+    }
+    if(!foodPlace[0].Opening_time && foodPlace[0].Days_open === "0123456") {
+      openTimeSentence = "Open 24/7"
+    }
+    else if(foodPlace[0].Opening_time && foodPlace[0].Days_open === "0123456") {
+      openTimeSentence = `Open everyday from ${openHourText} to ${closeHourText}`
+    }
+    else if(!foodPlace[0].Opening_time && foodPlace[0].Days_open !== "0123456") {
+      openTimeSentence = `Open 24 hours (${openWeekDays})`
+    }
+    else if(foodPlace[0].Opening_time && foodPlace[0].Days_open !== "0123456") {
+      openTimeSentence = `Open from ${openHourText} to ${closeHourText} (${openWeekDays})`
     }
   }
   
@@ -38,16 +88,19 @@ function FoodPlace({ match }) {
               </div>
           </div>
       </div>
-      <div class="rowcenter margin-tb-30">
-        <div class="col-5">
-            <img class="foodLargePic" src={foodPlace[0].Picture} alt={match.params.foodPlaceName}/>
+      <div className="rowcenter margin-tb-30">
+        <div className="col-5">
+          <img className="foodLargePic" src={foodPlace[0].Picture} alt={match.params.foodPlaceName}/>
         </div>
-        <div class="col-5">
-          <div class="margin-lr-20">
-            <div class="row">
+        <div className="col-5">
+          <div className="margin-lr-20">
+            <div className="row">
               <h4>{match.params.foodPlaceName}</h4>
               {placeClosed &&
-                <p style={{color: 'red'}}><strong>CLOSED AT THIS TIME</strong></p>
+                <div>
+                  <p style={{color: 'red'}}><strong>CLOSED AT THIS TIME</strong></p>
+                  <p>{nextOpenWhen}</p>
+                </div>
               }
             </div>
             <Ratings
@@ -63,25 +116,30 @@ function FoodPlace({ match }) {
 									<Ratings.Widget />
 									<Ratings.Widget />
 								</Ratings>
-            <div class="row">
+            <div className="row">
                 <p><strong>Price: </strong>{foodPlace[0].Price_range}</p>
             </div>
-            <div class="row">
+            <div className="row">
                 <p><strong>Location: </strong>{foodPlace[0].Location}</p>
             </div>
-            <div class="row">
+            <div className="row">
                 <p><strong>Food: </strong>{foodPlace[0].Food_types}</p>
             </div>
-            <div class="row margin-tb-10">
+            <div className="row margin-tb-10">
+                <p>{openTimeSentence}</p>
+            </div>
+            <div className="row margin-tb-10">
                 <p>{foodPlace[0].Description}</p>
             </div>
           </div>
         </div>
           
       </div>
-      <Rate />
       <JeepneyRoute Food_place_id={foodPlace[0].Food_place_id}/>
       <FoodPlaceMap latitude={foodPlace[0].Latitude} longitude={foodPlace[0].Longitude}/>
+      <div className="row">
+        <Rate />
+      </div>
     </div>
   );
 }
